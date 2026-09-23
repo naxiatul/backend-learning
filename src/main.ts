1c89e17f0279459377a54dfd6d1bd86abb6297ed
naxiatul <iqqa2112@gmail.com>
2026-09-23 19:40:08 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: 'http://localhost:5173' });
+   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
